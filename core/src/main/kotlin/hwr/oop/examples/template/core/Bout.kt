cd40407d6ac766cf5f81443191b2835c9c643955
Hendrik <hendrik.bernhardt07@gmail.com>
2026-05-07 14:01:15 +0200

    Fixed CardCompareTest and made syntax for the CardCompare better

--- core/src/main/kotlin/hwr/oop/examples/template/core/Bout.kt
+++ core/src/main/kotlin/hwr/oop/examples/template/core/Bout.kt
@@ -1,20 +1,5 @@
 package hwr.oop.examples.template.core
 
 class Bout {
-	companion object {
-		private var firstCard: Card? = null
-		private var secondCard: Card? = null
 
-		fun compareCards(firstCard: Card, secondCard: Card): Card {
-			this.firstCard = firstCard
-			this.secondCard = secondCard
-			if (firstCard.getCardValue(firstCard.rank()) > secondCard.getCardValue(secondCard.rank())) {
-				return firstCard
-			} else if (firstCard.getCardValue(firstCard.rank()) < secondCard.getCardValue(secondCard.rank())) {
-				return secondCard
-			} else {
-				throw Exception("Trump is not implemented yet")
-			}
-		}
-	}
 }
---
+++ core/src/main/kotlin/hwr/oop/examples/template/core/CardDefend.kt
@@ -0,0 +1,21 @@
+package hwr.oop.examples.template.core
+
+
+class CardDefend() {
+	companion object {
+		private var firstCard: Card? = null
+		private var secondCard: Card? = null
+		
+		fun compareCards(firstCard: Card, secondCard: Card): Card {
+			this.firstCard = firstCard
+			this.secondCard = secondCard
+			if (firstCard.getCardValue(firstCard.rank()) > secondCard.getCardValue(secondCard.rank())) {
+				return firstCard
+			} else if (firstCard.getCardValue(firstCard.rank()) < secondCard.getCardValue(secondCard.rank())) {
+				return secondCard
+			} else {
+				throw Exception("Trump is not implemented yet")
+			}
+		}
+	}
+}
